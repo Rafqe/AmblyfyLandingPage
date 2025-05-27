c1750fddf2c4be841c8212c4fc2919b24ba7bc41
Rafqe <apelisrafaels@gmail.com>
2025-05-27 11:05:16 +0300

    fix

--- backend/src/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const contactController = require("../controllers/contactController");
-const validateContact = require("../middleware/validateContact");
-
-router.post("/submit", validateContact, contactController.submitContact);
-
-module.exports = router;
