f12cc7ccc68683837406db12989eb6bd98a8e30c
Rafqe <apelisrafaels@gmail.com>
2025-06-03 11:28:20 +0300

    supabase update

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
   supabaseUrl:
     process.env.NEXT_PUBLIC_SUPABASE_URL ||
     "https://temkhtebkbcidecterqz.supabase.co",
-  supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "your-anon-key",
+  supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlbWtodGVia2JjaWRlY3RlcnF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5MzQwMDYsImV4cCI6MjA2NDUxMDAwNn0.6lK1CuirhFNRB-RVwMc-QPwirWlPUlNEbCjTPOg7B7E",
 };
 
 export default config;
